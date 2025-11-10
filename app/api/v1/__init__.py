d79947545a25a68dcb5c0656aae68aaad7473e1e
arit98 <aritradaschowdhury@gmail.com>
2025-11-11 04:41:22 +0530

    fix problem

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter
 from .auth import router as auth_router
-from .users import router as users_router
 from .trials import router as trials_router
 from .publications import router as publications_router
 from .experts import router as experts_router
@@ -9,7 +8,6 @@ from .favourites import router as favourites_router
 router = APIRouter()
 
 router.include_router(auth_router, prefix="/auth", tags=["auth"])
-router.include_router(users_router, prefix="/users", tags=["users"])
 router.include_router(trials_router, prefix="/trials", tags=["trials"])
 router.include_router(publications_router, prefix="/publications", tags=["publications"])
 router.include_router(experts_router, prefix="/experts", tags=["experts"])
